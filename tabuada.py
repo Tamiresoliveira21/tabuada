c6ea243e756ab8ae6d15d34a03eb4b8489a73bb5
Seu nome <seu-email@exemplo.com>
2023-04-15 23:51:57 -0300

    tabuada

---
+++ tabuada.py
@@ -0,0 +1,13 @@
+n= int(input('Digite um número:')) 
+
+print('{} x {}= {} '. format(n,1,n*1)) 
+
+print('{} x {}= {}'. format(n,2,n*2)) 
+
+print('{} x {}= {}'. format(n,3,n*3)) 
+
+print('{} x {}= {}'. format(n,4,n*4)) 
+
+print('{} x {}= {}'. format(n,5,n*5)) 
+
+print ('{} x {}= {}'. format(n,6,n*6)) 
